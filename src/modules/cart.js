99a8c585a53c76d629bc7224ae56c58953886665
DrumPire <expresproject@gmail.com>
2021-12-20 17:15:38 +0200

    added send cart on server and filters for price/hotSale

--- src/modules/cart.js
+++ src/modules/cart.js
@@ -1,18 +1,81 @@
+import renderCart from "./renderCart";
+import postData from "./postData";
+
 const cart = () => {
-    const cartBtn = document.getElementById('cart')
-    const cartModal = document.querySelector('.cart')
-    const cartCloseBtn = cartModal.querySelector('.cart-close')
+    const cartBtn = document.getElementById('cart'),
+          cartModal = document.querySelector('.cart'),
+          cartCloseBtn = cartModal.querySelector('.cart-close'),
+          cartTotal = cartModal.querySelector('.cart-total > span'),
+          cartSendBtn = cartModal.querySelector('.cart-confirm'),
+          cartWrapper = document.querySelector('.cart-wrapper'),
+          goodsWrapper = document.querySelector('.goods');
 
     const openCart = () => {
-        cartModal.style.display = 'flex'
-    }
+        const cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+
+        cartModal.style.display = 'flex';
+
+        renderCart(cart);
+
+        cartTotal.textContent = cart.reduce((sum, goodItem) => {
+            return sum + goodItem.price;
+        }, 0);
+    };
 
     const closeCart = () => {
-        cartModal.style.display = ''
-    }
+        cartModal.style.display = '';
+    };
 
-    cartBtn.addEventListener('click', openCart)
-    cartCloseBtn.addEventListener('click', closeCart)
-}
+    cartBtn.addEventListener('click', openCart);
+    cartCloseBtn.addEventListener('click', closeCart);
+    goodsWrapper.addEventListener('click', e => {
+        if (e.target.classList.contains('btn-primary')) {
+            const card = e.target.closest('.card'),
+                  key = card.dataset.key,
+                  goods = JSON.parse(localStorage.getItem('goods')),
+                  cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [],
+                  goodItem = goods.find(item => {
+                      return item.id === +key;
+                  });
 
-export default cart
+            cart.push(goodItem);
+            
+            localStorage.setItem('cart', JSON.stringify(cart));
+        }
+    });
+
+    cartWrapper.addEventListener('click', e => {
+        if (e.target.classList.contains('btn-primary')) {
+            const cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [],
+                  card = e.target.closest('.card'),
+                  key = card.dataset.key,
+                  index = cart.findIndex(item => {
+                    return item.id === +key;
+                  });
+
+            cart.splice(index, 1);
+
+            localStorage.setItem('cart', JSON.stringify(cart));
+
+            renderCart(cart);
+
+            cartTotal.textContent = cart.reduce((sum, goodItem) => {
+                return sum + goodItem.price;
+            }, 0);
+        }
+    });
+
+    cartSendBtn.addEventListener('click', () => {
+        const cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+
+        postData(cart).then(() => {
+            localStorage.removeItem('cart');
+
+            renderCart([]);
+
+            cartTotal.textContent = 0;
+        });
+    });
+};
+
+export default cart;
---
+++ src/modules/filter.js
@@ -0,0 +1,37 @@
+import getData from "./getData";
+import renderGoods from "./renderGoods";
+import { priceFilter, hotSaleFilter } from "./filters";
+
+const filter =() => {
+
+  const minInput = document.getElementById('min'),
+        maxInput = document.getElementById('max'),
+        checkboxInput = document.getElementById('discount-checkbox'),
+        checkboxSpan = document.querySelector('.filter-check_checkmark');
+
+  minInput.addEventListener('input', () => {
+    getData().then(data => {
+    renderGoods(priceFilter(hotSaleFilter(data, checkboxInput.checked), minInput.value, maxInput.value));
+    });
+  });
+
+  maxInput.addEventListener('input', () => {
+    getData().then(data => {
+    renderGoods(priceFilter(hotSaleFilter(data, checkboxInput.checked), minInput.value, maxInput.value));
+    });
+  });
+
+  checkboxInput.addEventListener('change', () => {
+    if (checkboxInput.checked) {
+      checkboxSpan.classList.add('checked');
+    } else {
+      checkboxSpan.classList.remove('checked');
+    }
+    getData().then(data => {
+      renderGoods(priceFilter(hotSaleFilter(data, checkboxInput.checked), minInput.value, maxInput.value));
+    });
+  });
+
+};
+
+export default filter;
